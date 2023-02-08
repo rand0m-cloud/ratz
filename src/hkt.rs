94005d67f9414c24487189eaccb0f83c72e46be8
Narek Asadorian <narek.asadorian@sap.com>
2023-02-08 10:36:27 -0800

    Rename as_member to reify

--- src/hkt.rs
+++ src/hkt.rs
@@ -1,7 +1,7 @@
 // HKTs with one parameter
 
 pub trait TypeConstructor1 {
-  type Of<A>: Mirror1<Constructor = Self, A = A>;
+  type K<A>: Mirror1<Constructor = Self, A = A>;
 }
 
 pub trait AppliedTypeConstructor1<A>: TypeConstructor1 {
@@ -9,21 +9,18 @@ pub trait AppliedTypeConstructor1<A>: TypeConstructor1 {
 }
 
 impl<F: TypeConstructor1, A> AppliedTypeConstructor1<A> for F {
-  type Mirror = Self::Of<A>;
+  type Mirror = Self::K<A>;
 }
 
 pub trait Mirror1: Sized {
-  type Constructor: AppliedTypeConstructor1<Self::A, Mirror = Self>;
-
   type A;
-
-  fn as_member(self) -> <Self::Constructor as TypeConstructor1>::Of<Self::A> {
+  type Constructor: AppliedTypeConstructor1<Self::A, Mirror = Self>;
+  fn reify(self) -> <Self::Constructor as TypeConstructor1>::K<Self::A> {
     fn witness<F: TypeConstructor1, A>(
       fa: <F as AppliedTypeConstructor1<A>>::Mirror,
-    ) -> F::Of<A> {
+    ) -> F::K<A> {
       fa
     }
-
     witness::<Self::Constructor, Self::A>(self)
   }
 }
@@ -31,7 +28,7 @@ pub trait Mirror1: Sized {
 // HKTs with two parameters
 
 pub trait TypeConstructor2 {
-  type Of<A, B>: Mirror2<Constructor = Self, A = A, B = B>;
+  type K<A, B>: Mirror2<Constructor = Self, A = A, B = B>;
 }
 
 pub trait AppliedTypeConstructor2<A, B>: TypeConstructor2 {
@@ -39,21 +36,19 @@ pub trait AppliedTypeConstructor2<A, B>: TypeConstructor2 {
 }
 
 impl<F: TypeConstructor2, A, B> AppliedTypeConstructor2<A, B> for F {
-  type Mirror = Self::Of<A, B>;
+  type Mirror = Self::K<A, B>;
 }
 
 pub trait Mirror2: Sized {
-  type Constructor: AppliedTypeConstructor2<Self::A, Self::B, Mirror = Self>;
-
   type A;
   type B;
-
-  fn as_member(
+  type Constructor: AppliedTypeConstructor2<Self::A, Self::B, Mirror = Self>;
+  fn reify(
     self,
-  ) -> <Self::Constructor as TypeConstructor2>::Of<Self::A, Self::B> {
+  ) -> <Self::Constructor as TypeConstructor2>::K<Self::A, Self::B> {
     fn witness<F: TypeConstructor2, A, B>(
       fa: <F as AppliedTypeConstructor2<A, B>>::Mirror,
-    ) -> F::Of<A, B> {
+    ) -> F::K<A, B> {
       fa
     }
     witness::<Self::Constructor, Self::A, Self::B>(self)
